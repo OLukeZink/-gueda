91c434e5355563f262206d754e8f1020199ada3c
OLukeZink <a25679@aepombal.edu.pt>
2025-10-06 09:24:38 +0100

    menu principal

---
+++ trabalhoxavieeu.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main()
+{
+    int opcao;
+
+    std::cout << "=== MENU ===\n";
+    std::cout << "1. Jogar\n";
+    std::cout << "2. Ver instruções\n";
+    std::cout << "3. Sair\n";
+    std::cout << "Escolha uma opção: ";
+    std::cin >> opcao;
+
+    switch(opcao) {
+        case 1:
+            std::cout << "Você escolheu Jogar!\n";
+            break;
+        case 2:
+            std::cout << "Instruções: Escolha uma opção do menu.\n";
+            break;
+        case 3:
+            std::cout << "Saindo do programa...\n";
+            break;
+        default:
+            std::cout << "Opção inválida!\n";
+    }
+
+    return 0;
+}
+
